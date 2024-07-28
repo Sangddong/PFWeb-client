711e8b541a21062d4445a7f42338100e4cae7524
sangddong <hehe74029@gmail.com>
2024-07-28 21:24:08 +0900

    feat: explore 버튼 추가

--- src/sections/main/Main.tsx
+++ src/sections/main/Main.tsx
@@ -1,10 +1,19 @@
+"use client";
 import LayoutPage from "@/components/layoutPage/LayoutPage";
 import Title from "@/components/sectionTitle/Title";
 import React from "react";
 import style from "./Main.module.css";
 import Banner from "../Banner/Banner";
+import Image from "next/image";
 
 function Main() {
+  const scrollToSection = () => {
+    const section = document.getElementById("about-me-wrap");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div id="main-wrap">
       <LayoutPage>
@@ -28,6 +37,17 @@ function Main() {
           <li id="3d"></li>
         </ul>
         <Banner />
+        <div className={style.explore} onClick={() => scrollToSection}>
+          <span>explore</span>
+          <div className={style.pointer}>
+            <Image
+              src="/img/pointer.png"
+              alt="pointer"
+              width={15}
+              height={15}
+            />
+          </div>
+        </div>
       </LayoutPage>
     </div>
   );
