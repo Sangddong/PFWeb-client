e5ab5f4f8a8ee51536aba4c715ecf5e60f5fa60a
sangddong <hehe74029@gmail.com>
2024-07-28 19:00:51 +0900

    feat: main화면 문구

--- src/sections/main/Main.tsx
+++ src/sections/main/Main.tsx
@@ -1,12 +1,31 @@
 import LayoutPage from "@/components/layoutPage/LayoutPage";
 import Title from "@/components/sectionTitle/Title";
 import React from "react";
+import style from "./Main.module.css";
 
 function Main() {
   return (
     <div id="main-wrap">
       <LayoutPage>
-        <Title>Hello</Title>
+        <ul className={style.main}>
+          <li>
+            <div className={style.welcome}>Welcome to My</div>
+            <div className={style.welcome}>
+              <span>Protfolio</span>
+              <span>Website</span>
+            </div>
+            <p>
+              Hi, I’m Sanghee Park.
+              <br />
+              I am a full-stack web developer based on Node.js.
+              <br />
+              Let&apos;s create wonderful websites together!
+              <br />
+              Thank you for visiting, and please hit the like button below!
+            </p>
+          </li>
+          <li id="3d"></li>
+        </ul>
       </LayoutPage>
     </div>
   );
