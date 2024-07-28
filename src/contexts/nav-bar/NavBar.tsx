86a01f63a69ddc157a49cc6c528bacbe03671bf8
sangddong <hehe74029@gmail.com>
2024-07-28 18:07:42 +0900

    feat: 햄버거 토글 구현

--- src/contexts/nav-bar/NavBar.tsx
+++ src/contexts/nav-bar/NavBar.tsx
@@ -1,22 +1,39 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import style from "./NavBar.module.css";
 
 function NavBar() {
+  const [sideBar, setSideBar] = useState(false);
+  const sideBarRef = useRef(null);
+
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
+    setSideBar(false);
   };
 
   const copyToClipBoard = async () => {
     await navigator.clipboard.writeText("hehe74029@gmail.com");
     alert("복사 완");
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sideBarRef.current && !sideBarRef.current.contains(event.target)) {
+        setSideBar(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={style.nav}>
@@ -29,44 +46,14 @@ function NavBar() {
         >
           Sangddong
         </li>
-        <li>
-          <p
-            onClick={() => {
-              scrollToSection("about-me-wrap");
-            }}
-          >
-            About Me
-          </p>
-          <p
-            onClick={() => {
-              scrollToSection("my-skills-wrap");
-            }}
-          >
-            My Skills
-          </p>
-          <p
-            onClick={() => {
-              scrollToSection("portfolio-wrap");
-            }}
-          >
-            Portfolio
-          </p>
-          <p
-            onClick={() => {
-              scrollToSection("my-sites-wrap");
-            }}
-          >
-            My Sites
-          </p>
-          <p
-            onClick={() => {
-              scrollToSection("comments-wrap");
-            }}
-          >
-            Comments
-          </p>
+        <li className={style.desktopMenu}>
+          <p onClick={() => scrollToSection("about-me-wrap")}>About Me</p>
+          <p onClick={() => scrollToSection("my-skills-wrap")}>My Skills</p>
+          <p onClick={() => scrollToSection("portfolio-wrap")}>Portfolio</p>
+          <p onClick={() => scrollToSection("my-sites-wrap")}>My Sites</p>
+          <p onClick={() => scrollToSection("comments-wrap")}>Comments</p>
         </li>
-        <li>
+        <li className={style.desktopIcons}>
           <Image
             src="/img/icons/mail.png"
             alt="mail"
@@ -83,7 +70,38 @@ function NavBar() {
             />
           </Link>
         </li>
+        <li className={style.menuIcon} onClick={() => setSideBar(!sideBar)}>
+          <img src="/img/menu.png" alt="menu" className={style.menuImg} />
+        </li>
       </ul>
+      {sideBar && (
+        <div className={style.sideBar} ref={sideBarRef}>
+          <div className={style.sideBarMenu}>
+            <p onClick={() => scrollToSection("about-me-wrap")}>About Me</p>
+            <p onClick={() => scrollToSection("my-skills-wrap")}>My Skills</p>
+            <p onClick={() => scrollToSection("portfolio-wrap")}>Portfolio</p>
+            <p onClick={() => scrollToSection("my-sites-wrap")}>My Sites</p>
+            <p onClick={() => scrollToSection("comments-wrap")}>Comments</p>
+          </div>
+          <div className={style.sideBarIcons}>
+            <Image
+              src="/img/icons/mail.png"
+              alt="mail"
+              width={35}
+              height={35}
+              onClick={copyToClipBoard}
+            />
+            <Link href={"https://github.com/Sangddong"} target="blank">
+              <Image
+                src="/img/icons/github-mark-white.png"
+                alt="github"
+                width={30}
+                height={30}
+              />
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
