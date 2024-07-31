d4a0fbb255480eb2d1ee510059005653eb548558
sangddong <hehe74029@gmail.com>
2024-07-31 18:59:29 +0900

    feat: 댓글 작성 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import NavBar from "@/contexts/nav-bar/NavBar";
 import AboutMe from "@/sections/aboutMe/AboutMe";
 import Comments from "@/sections/comments/Comments";
@@ -5,16 +6,21 @@ import Main from "@/sections/main/Main";
 import Portfolio from "@/sections/portfolio/Portfolio";
 import Sites from "@/sections/sites/Sites";
 import Skills from "@/sections/skills/Skills";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Home() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <NavBar />
-      <Main />
-      <AboutMe />
-      <Skills />
-      <Portfolio />
-      <Sites />
+      <QueryClientProvider client={queryClient}>
+        <NavBar />
+        <Main />
+        <AboutMe />
+        <Skills />
+        <Portfolio />
+        <Sites />
+        <Comments />
+      </QueryClientProvider>
     </>
   );
 }
--- src/sections/comments/Comments.tsx
+++ src/sections/comments/Comments.tsx
@@ -1,7 +1,20 @@
+import LayoutPage from "@/components/layoutPage/LayoutPage";
+import Title from "@/components/sectionTitle/Title";
 import React from "react";
+import style from "./Comments.module.css";
+import WriteComment from "./writeComment/WirteComments";
 
 function Comments() {
-  return <div id="comments-wrap">Comments</div>;
+  return (
+    <div id="comments-wrap">
+      <LayoutPage>
+        <div className={style.inner}>
+          <Title>Comments</Title>
+          <WriteComment />
+        </div>
+      </LayoutPage>
+    </div>
+  );
 }
 
 export default Comments;
